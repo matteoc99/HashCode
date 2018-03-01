473aa8947c9fe1455f9ffa74f42c17e07b10cbf5
Markus <markuspobitzer@gmail.com>
2018-03-01 18:18:58 +0100

    Filewriter

--- src/testbench/ReaderTest.java
+++ src/testbench/ReaderTest.java
@@ -2,10 +2,14 @@ package testbench;
 
 import utils.CodeUtils;
 
+import java.util.Arrays;
+
 public class ReaderTest {
 
     public static void main(String[] args) {
-        System.out.println(CodeUtils.readInString(System.getProperty("user.dir") + "/src/files/test.txt"));
+        char[][] zeichn = CodeUtils.readInArray(System.getProperty("user.dir") + "/src/files/test.txt");
+        System.out.println(Arrays.toString(zeichn[0]));
+        CodeUtils.writeArray(zeichn, "test.out");
 
     }
 }
--- src/utils/CodeUtils.java
+++ src/utils/CodeUtils.java
@@ -1,7 +1,10 @@
 package utils;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.Arrays;
 
 /**
  * @author Matteo Cosi
@@ -30,21 +33,42 @@ public class CodeUtils {
         return ret;
     }
 
-    public static String readInArray(String file){
-        String ret = "";
+    public static char[][] readInArray(String file){
+        char[][] ret = null;
         try(BufferedReader br = new BufferedReader(new FileReader(file))) {
-            StringBuilder sb = new StringBuilder();
             String line = br.readLine();
 
+            int lines = Integer.parseInt(line.substring(0, 1));
+            int zeichn = Integer.parseInt(line.substring(2, 3));
+            int zusatz1 = Integer.parseInt(line.substring(4, 5));
+            int zusatz2 = Integer.parseInt(line.substring(6, 7));
+
+            ret = new char[lines][zeichn];
+            int i = 0;
+            line = br.readLine();
             while (line != null) {
-                sb.append(line);
-                sb.append(System.lineSeparator());
+                for (int j = 0; j < zeichn; j++){
+                    char c = line.charAt(j);
+                    ret[i][j] = c;
+                }
+                i++;
+                System.out.println("\n");
                 line = br.readLine();
             }
-            ret = sb.toString();
         }catch (Exception e){
             e.printStackTrace();
         }
-        return ret;
+        return (ret != null)? ret : null;
+    }
+
+    public static void writeArray(char[][] data, String filename){
+        String name = System.getProperty("user.dir") + "/src/files/"+filename;
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(name))){
+            for(int i = 0; i < data.length; i++){
+                bw.write(Arrays.toString(data[i])+"\n");
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
